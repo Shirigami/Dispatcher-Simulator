ebbc1b70424c4403ee47a83d520dc1c842ae5e11
Kenneth <kmadrian03@gmail.com>
2016-05-05 03:10:11 -0600

    Proceso

--- SimuladorDispatcher/SolucionPackage/Proceso.cs
+++ SimuladorDispatcher/SolucionPackage/Proceso.cs
@@ -6,7 +6,67 @@ using System.Threading.Tasks;
 
 namespace SimuladorDispatcher.SolucionPackage
 {
-    class Proceso
+    public class Proceso
     {
+        static int cantInstancias = 0;
+        int id;
+        string estado;
+        int tiempo;
+        string tipo;
+
+        public Proceso(string estado, int tiempo, string tipo)
+        {
+            id = cantInstancias;
+            cantInstancias++;
+            this.Estado = estado;
+            this.Tiempo = tiempo;
+            this.Tipo = tipo;
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+        }
+        public string Estado
+        {
+            get
+            {
+                return estado;
+            }
+            set
+            {
+                estado = value;
+            }
+
+        }
+        public int Tiempo
+        {
+            get
+            {
+                return tiempo;
+            }
+            set
+            {
+                tiempo = value;
+            }
+
+        }
+        public string Tipo
+        {
+            get
+            {
+                return tipo;
+            }
+            set
+            {
+                tipo = value;
+            }
+
+        }
     }
+
+
 }
