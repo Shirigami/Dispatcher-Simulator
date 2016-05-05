45ee835639a7db25a2b643b7936e7314485445ae
xXMAMBOXx <mar09f@gmail.com>
2016-05-05 01:40:15 -0600

    wtf

--- SimuladorDispatcher/Program.cs
+++ SimuladorDispatcher/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SimuladorDispatcher;
 
 namespace SimuladorDispatcher
 {
@@ -16,7 +17,7 @@ namespace SimuladorDispatcher
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SolucionPackage.Form1());
+            Application.Run(new Form());
         }
     }
 }
